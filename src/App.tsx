bdfa7e212620db61ff3e499891f20b5d1751514d
Ashish <ashishyadav031001@gmail.com>
2025-12-26 11:22:20 +0530

    setup:project structure setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>App is working</Text>
     </View>
   );
 };
